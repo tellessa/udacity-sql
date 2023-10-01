fd8cb25027124024bfc03daa64eafd67d0b100e7
tellessa <76462081+tellessa@users.noreply.github.com>
2023-09-30 20:24:47 -0700

    Analyzed why answer 3 is what it is

--- SQL Subqueries and Table Expressions/Quiz- Subquery Mania/3- answer.sql
+++ SQL Subqueries and Table Expressions/Quiz- Subquery Mania/3- answer.sql
@@ -20,3 +20,8 @@ FROM (
                     ) inner_tab
             )
     ) counter_tab;
+-- We can use a column in the Having clause that we didn't use in the SELECT clause, but we must group by something in the select for that to even make sense.
+-- The innermost table gives us the account names and their sums of standard qtys and totals. standard_qty is only included so that it can be used to order from greatest to least by that column. After the ordering is done, we only want the top row, which is what this table's result set ends up being limited to.
+-- Having gotten the top row, we actually throw away the act name and tot_std from the second-to-deepest query, selecting only the total column.
+-- The second-to-highest query selects the account name and groups by it. Grouping in this way filters the a.name to the number of unique names there are. The HAVING clause further filters the result set to only the rows where the sum of the total column is greater than the second-to-deepest queries result. Remember, that query gave us the total for the account with the highest tot_std.
+-- Finally, the outermost query just gives a simple count of the number of rows returned by the second-to-highest query.
