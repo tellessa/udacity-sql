9938879ba563dce7a4565c335845de18defae006
tellessa <76462081+tellessa@users.noreply.github.com>
2023-10-19 15:08:50 -0700

    Adds 3rd and final percentile quiz solution

--- SQL Window Functions/Quiz- Percentiles/3.sql
+++ SQL Window Functions/Quiz- Percentiles/3.sql
@@ -2,3 +2,22 @@
 -- the occurred_at time for each order,
 -- the total amount of total_amt_usd paper purchased,
 -- and one of 100 levels in a total_percentile column.
+SELECT account_id,
+    occurred_at,
+    total_amt_usd,
+    NTILE(100) OVER (
+        PARTITION BY account_id
+        ORDER BY gloss_qty
+    ) AS gloss_percentile
+FROM orders
+ORDER BY account_id DESC;
+-- Answer: I accidentally made a gloss percentile instead of doing it for total amt usd
+SELECT account_id,
+    occurred_at,
+    total_amt_usd,
+    NTILE(100) OVER (
+        PARTITION BY account_id
+        ORDER BY total_amt_usd
+    ) AS total_percentile
+FROM orders
+ORDER BY account_id DESC
