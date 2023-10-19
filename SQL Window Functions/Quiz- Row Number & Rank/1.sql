aa00deeab70b0326f0b11bb1edf31908d74c37f1
tellessa <76462081+tellessa@users.noreply.github.com>
2023-10-18 17:53:44 -0700

    Adds answer for RANK quiz

--- SQL Window Functions/Quiz- Row Number & Rank/1.sql
+++ SQL Window Functions/Quiz- Row Number & Rank/1.sql
@@ -6,4 +6,23 @@ SELECT id,
         PARTITION BY account_id
         ORDER BY DATE_TRUNC('month', occurred_at)
     ) AS row_num
-FROM orders
+FROM orders -- Select the id, account_id, and total variable from the orders table, then create a column called total_rank that ranks this total amount of paper ordered (from highest to lowest) for each account using a partition. Your final table should have these four columns.
+    -- My answer:
+SELECT o.id,
+    o.account_id,
+    o.total,
+    RANK() OVER (
+        PARTITION BY account_id
+        ORDER BY total DESC
+    ) AS total_rank
+FROM orders o
+    JOIN accounts a ON o.account_id = a.id;
+-- Answer:
+SELECT id,
+    account_id,
+    total,
+    RANK() OVER (
+        PARTITION BY account_id
+        ORDER BY total DESC
+    ) AS total_rank
+FROM orders;
