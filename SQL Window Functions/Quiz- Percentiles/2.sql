ddccdfc6069f81a930853b405d857a0d6c825487
tellessa <76462081+tellessa@users.noreply.github.com>
2023-10-19 15:05:35 -0700

    Adds percentiles 2 answer

--- SQL Window Functions/Quiz- Percentiles/2.sql
+++ SQL Window Functions/Quiz- Percentiles/2.sql
@@ -2,3 +2,22 @@
 -- the occurred_at time for each order,
 -- the total amount of gloss_qty paper purchased,
 -- and one of two levels in a gloss_half column.
+SELECT account_id,
+    occurred_at,
+    gloss_qty,
+    NTILE(2) OVER (
+        PARTITION BY account_id
+        ORDER BY gloss_qty
+    )
+FROM orders
+ORDER BY account_id DESC;
+-- Answer:
+SELECT account_id,
+    occurred_at,
+    gloss_qty,
+    NTILE(2) OVER (
+        PARTITION BY account_id
+        ORDER BY gloss_qty
+    ) AS gloss_half
+FROM orders
+ORDER BY account_id DESC
